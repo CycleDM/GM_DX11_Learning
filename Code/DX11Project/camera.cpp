aed225ea7c79e3a0fb10f4ae5b4da18cafcf0eba
CycleDM <cycledm0508@gmail.com>
2021-05-10 11:47:40 +0900

    Init

---
+++ Code/DX11Project/camera.cpp
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "renderer.h"
+#include "manager.h"
+#include "camera.h"
+
+void Camera::Init()
+{
+	m_Position = D3DXVECTOR3(0.0f, 2.0f, -5.0f);
+	m_Target = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+}
+
+void Camera::Uninit()
+{
+
+}
+
+void Camera::Update()
+{
+
+}
+
+void Camera::Draw()
+{
+	// ビューマトリクス設定
+	D3DXMATRIX viewMatrix;
+	D3DXMatrixLookAtLH(&viewMatrix, &m_Position, &m_Target, &D3DXVECTOR3(0.0f, 1.0f, 0.0f));
+
+	Renderer::SetViewMatrix(&viewMatrix);
+
+	// プロジェクションマトリクス設定
+	D3DXMATRIX projectionMatrix;
+	D3DXMatrixPerspectiveFovLH(&projectionMatrix, 1.0f, (float)SCREEN_WIDTH / SCREEN_HEIGHT, 1.0f, 1000.0f);
+
+	Renderer::SetProjectionMatrix(&projectionMatrix);
+}
